24b72b0925ad435f0aaef7c55f256101690d71c0
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-10-14 12:57:00 +0100

    Update main Gui

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os 
-from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QLineEdit,QPushButton,QFileDialog,QMessageBox
+from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QLineEdit,QPushButton,QFileDialog,QMessageBox,QTableWidget,QListWidget
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
@@ -100,6 +100,22 @@ class Generator(QWidget):
             try :
                 os.mkdir(path=self.path+"/"+str(self.project_name.text()), mode=777)
                 self.full_folder = self.path+"/"+str(self.project_name.text())
+                #folder created : 
+                # step 2 : create files : html/Css/Js 
+                print(self.full_folder)
+                for target_file in ['Index.html','Index.css','Index.js']:
+                    try:
+                        current_file = open(str(self.full_folder)+"/"+str(target_file), mode="w") 
+                    except Exception as e:
+                        print(" [ create file Error ] : "+str(e))
+                print(" All files created :)  ......")
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Create Project Notification ! ") 
+                msg.setInformativeText("Your  project created  ")
+                msg.setStandardButtons(QMessageBox.Ok)
+                msg.exec_()
+                self.create_section_handler()
             except Exception as e:
                 print("[ Create Folder Error : ] : "+str(e))
         else:
@@ -110,7 +126,68 @@ class Generator(QWidget):
             msg.setStandardButtons(QMessageBox.Ok)
             msg.exec_()
         return
+    def create_section_handler(self):
+        # add an custom section to manage page sections and style :
+        self.table_section_header = QLabel(self)
+        self.table_section_header.setText(" Your sections :   ")
+        self.table_section =  QListWidget(self)
+        self.table_section.addItem(" Body ")
+        self.button_open_section = QPushButton(self)
+        self.button_open_section.setText(" Open ")
+        self.button_open_section.setCursor(Qt.PointingHandCursor)
+        self.button_open_section.setEnabled(False)
 
+        self.button_Add_section = QPushButton(self)
+        self.button_Add_section.setText(" Add new section  ")
+        self.button_Add_section.setCursor(Qt.PointingHandCursor)
+
+        self.button_delete_section = QPushButton(self)
+        self.button_delete_section.setText(" Remove ")
+        self.button_delete_section.setCursor(Qt.PointingHandCursor)
+        self.button_delete_section.setEnabled(False)
+        # add styles : 
+        self.table_section_header.setStyleSheet("""
+            font-size : 20px;
+            color:"#fff";
+            border:none;
+            """)
+        self.table_section.setStyleSheet("""
+            font-size : 12px;
+            color:"#fff";
+            border:1px solid #FFD60A;
+            padding:10px 10px;
+            border-radius: 15px;
+            width:250px;
+            """)
+        self.button_Add_section.setStyleSheet("""
+            font-size : 12px;
+            color:"#fff";
+            border:none;
+            """)
+        self.button_delete_section.setStyleSheet("""
+            font-size : 12px;
+            color:"#003566";
+            border:none;
+            """)
+        self.button_open_section.setStyleSheet("""
+            font-size : 12px;
+            color:"#fff";
+            border:none;
+            """)
+        # place widgets : 
+        self.table_section_header.move(150,320)
+        self.table_section.move(150,360)
+        self.button_open_section.move(150,580)
+        self.button_Add_section.move(200,580)
+        self.button_delete_section.move(360,580)
+        # set show attribute : 
+        self.table_section_header.show()
+        self.table_section.show()
+        self.button_open_section.show()
+        self.button_Add_section.show()
+        self.button_delete_section.show()
+        return 
+    
 if __name__ == "__main__":
     instance = QApplication.instance()
     if not instance:
